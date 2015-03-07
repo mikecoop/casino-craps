002410d9bc1a2d7a3cc18332c65b2260706dbbcc
Mike Cooper <coop87@gmail.com>
2015-03-07 11:08:09 -0500

    Add initial project structure.

---
+++ GoF.CasinoCraps.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoF.CasinoCraps.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(true);
+        }
+    }
+}
