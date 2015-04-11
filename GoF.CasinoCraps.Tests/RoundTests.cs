80b8b289fdf4d5fdeb48f0244e39d59b147f8070
Mike Cooper <coop87@gmail.com>
2015-04-11 11:27:35 -0400

    Implements #7. Add round events and supporting logic.

--- GoF.CasinoCraps.Tests/RoundTests.cs
+++ GoF.CasinoCraps.Tests/RoundTests.cs
@@ -24,7 +24,7 @@ namespace GoF.CasinoCraps.Tests
         {
             Round round = new Round();
 
-            round.NextRoll(new Roll(3, 1));
+            round.SetNextRoll(new Roll(3, 1));
             
             round.Phase.Should().Be(RoundPhase.Point);
         }
@@ -34,7 +34,7 @@ namespace GoF.CasinoCraps.Tests
         {
             Round round = new Round();
 
-            round.NextRoll(new Roll(1, 4));
+            round.SetNextRoll(new Roll(1, 4));
 
             round.Phase.Should().Be(RoundPhase.Point);
         }
@@ -44,7 +44,7 @@ namespace GoF.CasinoCraps.Tests
         {
             Round round = new Round();
 
-            round.NextRoll(new Roll(3, 3));
+            round.SetNextRoll(new Roll(3, 3));
 
             round.Phase.Should().Be(RoundPhase.Point);
         }
@@ -54,7 +54,7 @@ namespace GoF.CasinoCraps.Tests
         {
             Round round = new Round();
 
-            round.NextRoll(new Roll(3, 5));
+            round.SetNextRoll(new Roll(3, 5));
 
             round.Phase.Should().Be(RoundPhase.Point);
         }
@@ -64,7 +64,7 @@ namespace GoF.CasinoCraps.Tests
         {
             Round round = new Round();
 
-            round.NextRoll(new Roll(4, 5));
+            round.SetNextRoll(new Roll(4, 5));
 
             round.Phase.Should().Be(RoundPhase.Point);
         }
@@ -74,9 +74,155 @@ namespace GoF.CasinoCraps.Tests
         {
             Round round = new Round();
 
-            round.NextRoll(new Roll(6, 4));
+            round.SetNextRoll(new Roll(6, 4));
 
             round.Phase.Should().Be(RoundPhase.Point);
+        }
+
+        [Test]
+        public void SetNextRoll_PointEstablished_RaisesPointEstablishedEvent()
+        {
+            Round round = new Round();
+
+            round.MonitorEvents();
+
+            round.SetNextRoll(new Roll(6, 4));
+
+            round.ShouldRaise("PointEstablished")
+                .WithSender(round);
+        }
+
+        [Test]
+        public void PointValue_ComeOutPhase_IsZero()
+        {
+            Round round = new Round();
+
+            round.PointValue.Should().Be(0);
+        }
+
+        [Test]
+        public void PointValue_PointEstablished_ReturnsCorrectValue()
+        {
+            Round round = new Round();
+            
+            round.SetNextRoll(new Roll(5, 5));
+
+            round.PointValue.Should().Be(10);
+        }
+
+        [Test]
+        public void SetNextRoll_TwoRolled_RaisesRoundEndedEvent()
+        {
+            Round round = new Round();
+
+            round.MonitorEvents();
+
+            round.SetNextRoll(new Roll(1, 1));
+
+            round.ShouldRaise("RoundEnded")
+                .WithSender(round)
+                .WithArgs<RoundEndedEventArgs>(args => args.Result == RoundResult.Craps);
+        }
+
+        [Test]
+        public void SetNextRoll_ThreeRolled_RaisesRoundEndedEvent()
+        {
+            Round round = new Round();
+
+            round.MonitorEvents();
+
+            round.SetNextRoll(new Roll(2, 1));
+
+            round.ShouldRaise("RoundEnded")
+                .WithSender(round)
+                .WithArgs<RoundEndedEventArgs>(args => args.Result == RoundResult.Craps);
+        }
+
+        [Test]
+        public void SetNextRoll_TwelveRolled_RaisesRoundEndedEvent()
+        {
+            Round round = new Round();
+
+            round.MonitorEvents();
+
+            round.SetNextRoll(new Roll(6, 6));
+
+            round.ShouldRaise("RoundEnded")
+                .WithSender(round)
+                .WithArgs<RoundEndedEventArgs>(args => args.Result == RoundResult.Craps);
+        }
+
+        [Test]
+        public void SetNextRoll_TwoRolledAfterPointEstablished_NoRoundEndedEventRaised()
+        {
+            Round round = new Round();
+
+            round.MonitorEvents();
+
+            round.SetNextRoll(new Roll(3, 5));
+            round.SetNextRoll(new Roll(1, 1));
+
+            round.ShouldNotRaise("RoundEnded");
+        }
+
+        [Test]
+        public void SetNextRoll_SevenRolled_RaisesRoundEndedEvent()
+        {
+            Round round = new Round();
+
+            round.MonitorEvents();
+
+            round.SetNextRoll(new Roll(6, 1));
+
+            round.ShouldRaise("RoundEnded")
+                .WithSender(round)
+                .WithArgs<RoundEndedEventArgs>(args => args.Result == RoundResult.Natural);
+        }
+
+        [Test]
+        public void SetNextRoll_ElevenRolled_RaisesRoundEndedEvent()
+        {
+            Round round = new Round();
+
+            round.MonitorEvents();
+
+            round.SetNextRoll(new Roll(6, 5));
+
+            round.ShouldRaise("RoundEnded")
+                .WithSender(round)
+                .WithArgs<RoundEndedEventArgs>(args => args.Result == RoundResult.Natural);
+        }
+
+        [Test]
+        public void SetNextRoll_SevenPointEstablished_RaisesRoundEndedEvent()
+        {
+            Round round = new Round();
+
+            round.MonitorEvents();
+
+            round.SetNextRoll(new Roll(5, 1));
+
+            round.SetNextRoll(new Roll(6, 1));
+
+            round.ShouldRaise("RoundEnded")
+                .WithSender(round)
+                .WithArgs<RoundEndedEventArgs>(args => args.Result == RoundResult.SevenOut);
+        }
+
+        [Test]
+        public void SetNextRoll_PointHitAfterPointEstablished_RaisesRoundEndedEvent()
+        {
+            Round round = new Round();
+
+            round.MonitorEvents();
+
+            round.SetNextRoll(new Roll(5, 1));
+
+            round.SetNextRoll(new Roll(5, 1));
+
+            round.ShouldRaise("RoundEnded")
+                .WithSender(round)
+                .WithArgs<RoundEndedEventArgs>(args => args.Result == RoundResult.PointHit);
         }
     }
 }
--- GoF.CasinoCraps/Round.cs
+++ GoF.CasinoCraps/Round.cs
@@ -11,6 +11,10 @@ namespace GoF.CasinoCraps
     /// </summary>
     public class Round
     {
+        private readonly List<int> CrapsRolls = new List<int> { 2, 3, 12 };
+        private readonly List<int> NaturalRolls = new List<int> { 7, 11 };
+        private readonly List<int> PointRolls = new List<int> { 4, 5, 6, 8, 9, 10 };
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Round"/> class.
         /// </summary>
@@ -19,20 +23,62 @@ namespace GoF.CasinoCraps
             Phase = RoundPhase.ComeOut;
         }
 
+        public event EventHandler PointEstablished;
+
+        public event EventHandler<RoundEndedEventArgs> RoundEnded;
+
+        public int PointValue { get; private set; }
+
         public RoundPhase Phase { get; private set; }
 
-        public void NextRoll(Roll roll)
+        public void SetNextRoll(Roll roll)
         {
             Contract.Requires(roll != null);
 
-            if (roll.DiceTotal == 4 ||
-                roll.DiceTotal == 5 ||
-                roll.DiceTotal == 6 ||
-                roll.DiceTotal == 8 ||
-                roll.DiceTotal == 9 ||
-                roll.DiceTotal == 10)
+            if (Phase == RoundPhase.ComeOut)
             {
-                Phase = RoundPhase.Point;
+                if (CrapsRolls.Contains(roll.DiceTotal))
+                {
+                    EndRound(RoundResult.Craps);
+                }
+                else if (NaturalRolls.Contains(roll.DiceTotal))
+                {
+                    EndRound(RoundResult.Natural);
+                }
+                else if (PointRolls.Contains(roll.DiceTotal))
+                {
+                    EstablishPoint(roll);
+                }
+            }
+            else
+            {
+                if (roll.DiceTotal == 7)
+                {
+                    EndRound(RoundResult.SevenOut);
+                }
+                else if (roll.DiceTotal == PointValue)
+                {
+                    EndRound(RoundResult.PointHit);
+                }
+            }
+        }
+
+        private void EstablishPoint(Roll roll)
+        {
+            if (PointEstablished != null)
+            {
+                PointEstablished(this, new EventArgs());
+            }
+
+            Phase = RoundPhase.Point;
+            PointValue = roll.DiceTotal;
+        }
+
+        private void EndRound(RoundResult result)
+        {
+            if (RoundEnded != null)
+            {
+                RoundEnded(this, new RoundEndedEventArgs(result));
             }
         }
     }
