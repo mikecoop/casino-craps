b5aaf2be27410d407d57b84b9224f5451401905e
Mike Cooper <coop87@gmail.com>
2015-04-10 23:17:00 -0400

    Add missing test file

---
+++ GoF.CasinoCraps.Tests/RoundTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using GoF.CasinoCraps;
+using FluentAssertions;
+
+namespace GoF.CasinoCraps.Tests
+{
+    public class RoundTests
+    {
+        [Test]
+        public void Phase_WhenCreated_IsComeOut()
+        {
+            Round round = new Round();
+
+            round.Phase.Should().Be(RoundPhase.ComeOut);
+        }
+
+        [Test]
+        public void Phase_FourPointRolled_IsPoint()
+        {
+            Round round = new Round();
+
+            round.NextRoll(new Roll(3, 1));
+            
+            round.Phase.Should().Be(RoundPhase.Point);
+        }
+
+        [Test]
+        public void Phase_FivePointRolled_IsPoint()
+        {
+            Round round = new Round();
+
+            round.NextRoll(new Roll(1, 4));
+
+            round.Phase.Should().Be(RoundPhase.Point);
+        }
+
+        [Test]
+        public void Phase_SixPointRolled_IsPoint()
+        {
+            Round round = new Round();
+
+            round.NextRoll(new Roll(3, 3));
+
+            round.Phase.Should().Be(RoundPhase.Point);
+        }
+
+        [Test]
+        public void Phase_EightPointRolled_IsPoint()
+        {
+            Round round = new Round();
+
+            round.NextRoll(new Roll(3, 5));
+
+            round.Phase.Should().Be(RoundPhase.Point);
+        }
+
+        [Test]
+        public void Phase_NinePointRolled_IsPoint()
+        {
+            Round round = new Round();
+
+            round.NextRoll(new Roll(4, 5));
+
+            round.Phase.Should().Be(RoundPhase.Point);
+        }
+
+        [Test]
+        public void Phase_TenPointRolled_IsPoint()
+        {
+            Round round = new Round();
+
+            round.NextRoll(new Roll(6, 4));
+
+            round.Phase.Should().Be(RoundPhase.Point);
+        }
+    }
+}
